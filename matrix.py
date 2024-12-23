c3bc259b046adb36ec34265a3498a91d3ddc119a
Ansah-98 <yungtupac99@gmail.com>
2024-12-23 05:41:11 +0000

    unfiniished eigen value problem

--- matrix.py
+++ matrix.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Help_Funtion:
     def delete(self,matrix, col_index):
@@ -16,43 +17,68 @@ class Help_Funtion:
         determinant = 0
         for i in range(len(matrix)):
                 element   = matrix[0][i]
-
                 new_matrix  = self.delete(matrix,i)
                 #recursive function to trim down the matrix till its 2x2 and then we compute the determinant
-                determinant += ((-1)**(i)) * element *self.compute_determinant(new_matrix )
-                
+                determinant += ((-1)**(i)) * element *self.compute_determinant(new_matrix )  
         return determinant
-                
+
+
+#make_simlar: used to create a similar function using the QR decomposition
+#basically decomposing the matrix and finding  multiple similar matrices 
+#leaving only the eigen values in the diagonal and forming an upper triangular matrix
+    def make_similar(matrix):
+        matrix  = np.array(matrix)
+        sim_matrix = np.linalg.qr(matrix)
+        return sim_matrix
+
+#compute_eigenvalue:returns a list of all the eigen_values of a matrix
+    def gen_eigenvalue(self , matrix):
+        sim_matrix  = Help_Funtion.make_similar(matrix)
+        leig = sim_matrix[-1][-1] #gets the last eigen value in the similar matrix 
+        diff  = 1
+
+#iterate till the last eigen value's in the newest similar matrix
+# precision is less than 10^-32 compared to the eigen value before it
+        while diff > 1e-32: 
+            sim_matrix  = Help_Funtion.make_similar(sim_matrix)
+            diff = abs(leig - sim_matrix[-1][-1])
+            leig =sim_matrix[-1][-1]
+        eigs = [sim_matrix[i,i] for i in range(len(sim_matrix))]
+        return eigs
+
+
 class Matrix(Help_Funtion):
-    row = 0
-    column  = 0
+    row = None
+    column  = None
     matrix  = []
     def make_matrix(self):
-        for i in self.row:
-            print("enter your matrix row by row")
-            row = 0
-            col = 0
-            for i in range(self.row):
-                row  = []
-                for i in range(self.column):
-                    element  = int(input(f"enter the element at row {i + 1},  column {col + 1}"))
-                    row.append(element)
-                self.matrix.append(row)
+        self.row = int(input("enter the number of rows of the matrix"))
+        self.column = int(input("enter the number of columns of the matrix"))
+        
+        print("enter your matrix row by row")
+        for i in range(self.row):
+            row  = []
+            for i in range(self.column):
+                element  = int(input(f"enter the element at row {i + 1},  column {col + 1}"))
+                row.append(element)
+            self.matrix.append(row)
 
     def check_square_matrix(self):
         if len(self.row) == len(self.column)  and (self.row !=0):
             return True
-
-    def determinant(self,):
+    #main function for the determinant
+    def determinant(self):
         if len(self.matrix  == 1) and self.check_square_matrix():
             return self.matrix[0]
         if self.check_square_matrix():
             return self.compute_determinant(self.matrix)
     #function to delete the rows and columns of the element including the element
-    def form_matrix(matrix):
-        pass
-    #matrix.del()
+
+    def compute_eigenvalues(self):
+        if self.check_square_matrix():
+            return self.gen_eigenvalue(self.matrix)
 
 model = Help_Funtion()
-determinant  = model.compute_determinant([[1,2,4,8],[1,3,7,10],[3,4,5,18],[1,2,2,2]])
-print(determinant)
+determinant  = model.compute_determinant([[1,2,3],[3,1,2],[0,0,1]])
+eigen_values   = model.gen_eigenvalue([[1,2,3],[3,1,2],[0,0,1]])
+print(eigen_values)
